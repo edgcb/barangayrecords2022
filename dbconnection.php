b0d63135f2f35959198683fd060e9dbdd75a11a3
edgcb <67811120+edgcb@users.noreply.github.com>
2022-03-08 11:40:50 +0800

    Update dbconnection.php

--- dbconnection.php
+++ dbconnection.php
@@ -1,9 +1,9 @@
 <?php
-$server = "localhost";
-$username = "root";
-$password = "";
-$dbname = "capstone";
-$port = "3307";
+$server = "dcrhg4kh56j13bnu.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$username = "y5q14t2f9zv0wy8o";
+$password = "hcdazye08ydgxven";
+$dbname = "ayldjmh4a1830v8y";
+$port = "3306";
 
 
 $conn = new mysqli($server, $username, $password, $dbname, $port);
